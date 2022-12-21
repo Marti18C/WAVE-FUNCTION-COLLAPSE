716b71aeee64b5062d8a9a39f335f3b99fb610bf
Martina Carrasco <martinacarrascop@gmail.com>
2022-12-21 16:45:06 -0500

    AVANCE HASTA VIDEO 6.2

--- sketch.js
+++ sketch.js
@@ -1,7 +1,103 @@
+const celdas = []; // 4x4
+const RETICULA = 4;
+
+const azulejos = [];
+const NA = 11; // numero dse azulejos
+
+const reglas = [
+  //reglas de los bordes de cada azulejo
+{
+  // tile 0
+  UP: 0,
+  RIGHT: 0,
+  DOWN: 0,
+  LEFT: 0,
+},
+{
+  // tile 1
+  UP: 1,
+  RIGHT: 1,
+  DOWN: 1,
+  LEFT: 0,
+},
+{
+  // tile 2
+  UP: 0,
+  RIGHT: 1,
+  DOWN: 1,
+  LEFT: 1,
+},
+{
+  // tile 3
+  UP: 1,
+  RIGHT: 1,
+  DOWN: 0,
+  LEFT: 1,
+},
+{
+  // tile 4
+  UP: 1,
+  RIGHT: 0,
+  DOWN: 1,
+  LEFT: 1,
+},
+{
+  // tile 5
+  UP: 1,
+  RIGHT: 0,
+  DOWN: 0,
+  LEFT: 1,
+},
+{
+  // tile 6
+  UP: 1,
+  RIGHT: 1,
+  DOWN: 0,
+  LEFT: 0,
+},
+{
+  // tile 7
+  UP: 0,
+  RIGHT: 1,
+  DOWN: 1,
+  LEFT: 0,
+},
+{
+  // tile 8
+  UP: 0,
+  RIGHT: 0,
+  DOWN: 1,
+  LEFT: 1,
+},
+{
+  // tile 9
+  UP: 1,
+  RIGHT: 1,
+  DOWN: 1,
+  LEFT: 1,
+},
+{
+  // tile 10
+  UP: 0,
+  RIGHT: 0,
+  DOWN: 0,
+  LEFT: 0,
+},
+
+
+];
+
+function preload(){
+  for(let i = 0; i < NA; i++){
+  azulejos [i] = loadImage (`IMG/TILE${i}.png`);
+  }
+}
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  circle(mouseX, mouseY, 20);
+ 
 }
