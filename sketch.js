75b68d42c6bd9c9f9e4d9133925fb634b8bc68ea
Marti18 <martinacarrascop@gmail.com>
2023-01-02 19:41:39 -0500

    AVANCE HASTA VIDEO 6.6

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
 const celdas = []; // 4x4
-const RETICULA = 4;
+const RETICULA = 4; 
+let ancho; //ancho de celda
+let alto; //alto de celda
 
 const azulejos = [];
 const NA = 11; // numero de azulejos
@@ -97,7 +99,10 @@ function preload(){
 function setup() {
   createCanvas(1080, 1080);
 
-  let opcionesI = []
+  ancho = width / RETICULA;
+  alto = height / RETICULA;
+
+  let opcionesI = [];
   for (let i = 0; 1 < azulejos.length; i ++){
     opcionesI.push (i);
   }
@@ -108,9 +113,59 @@ function setup() {
       opciones: opcionesI,
     };
 }
-prin (celdas);
+//celdas [8].colapsada = true;
+//celdas [3].colapsada = true;
+
+//celdas [12].opciones = [5,6,8];
+//celdas [4].opciones = [4,7,12];
+//celdas [6].opciones = [9,7,12];
+//celdas [1].opciones = [6,4,8,10];
+//celdas [5].opciones = [11,6,4,8,10];
+
 }
 
 function draw() {
- 
+  background(111);
+
+const celdasDisponibles = celdas.filter ((celda) => {return celda.colapsada == false;
+});
+
+if (celdasDisponibles.length > 0 ){
+  celdasDisponibles.sort ((a,b) => {
+  return a.opciones.lenght - b.opciones.lenght;
+});
+
+const celdasPorColapsar = celdasDisponibles.filter((celda)=>{
+  return celda.opciones.lenght == celdasDisponibles[0].opciones.lenght
+});
+
+const celdaSeleccionada = random(celdasPorColapsar);
+celdaSeleccionada.colapsada = true //;
+
+const opcionSeleccionada = random (celdaSeleccionada.opciones);
+celdaSeleccionada.opciones = [opcionSeleccionada];
+
+
+
+print (celdaSeleccionada);
+
+
+
+for (let x = 0; x < RETICULA; x++ ){
+  for (let y = 0; y < RETICULA; y++ ){
+    const celdaIndex = x + y * RETICULA; 
+    const celdaActual = celdas[celdaIndex];
+    if(celdaActual.colapsada){
+        image(
+          azulejos[celdaActual.opciones[0]],x*ancho,
+          y*alto, 
+          ancho, 
+          alto
+          );
+    }
+
+  }
+}
+//noLoop ();
+}
 }
